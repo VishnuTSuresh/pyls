e4250250c587be2534715d5a886971535326064f
Vishnu T Suresh <vishnutsuresh@live.com>
2024-12-17 08:26:17 +0530

    completed subtask 2 and added tests

--- pyls/__main__.py
+++ pyls/__main__.py
@@ -1,7 +1,18 @@
 import json
+import argparse
+
 from pyls.ls import ls
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Python ls implementation")
+    parser.add_argument("-A", action="store_true")
+    args = parser.parse_args()
+
     with open("structure.json") as structure_file:
         structure_data = json.load(structure_file)
-        print(ls(structure_data))
+        print(
+            ls(
+                structure_data,
+                a=args.A
+            )
+        )
