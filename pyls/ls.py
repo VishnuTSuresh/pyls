4bb1776ff4d42244f529adbd9c3d1019f2a0f367
Vishnu T Suresh <vishnutsuresh@live.com>
2024-12-17 09:25:34 +0530

    Added functionality and unit tests

--- pyls/ls.py
+++ pyls/ls.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 
 
-def ls(structure_data, a=False, l=False, r=False):
+def ls(structure_data, a=False, l=False, r=False, t=False):
     contents = structure_data["contents"].copy()
+    if t:
+        contents = sorted(contents, key=lambda content: content["time_modified"])
     if r:
         contents = reversed(contents)
     if not a:
@@ -14,5 +16,5 @@ def ls(structure_data, a=False, l=False, r=False):
     else:
         rows = [f"{content['permissions']} {content["size"]} {datetime.fromtimestamp(content['time_modified']).strftime('%b %d %H:%M')} {content["name"]}" for content in contents]
         result = "\n".join(rows)
-    
+
     return result
