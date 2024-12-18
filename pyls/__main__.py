1befa349e672f048042893ae10a9ed7be0ff8ab5
Vishnu T Suresh <vishnutsuresh@live.com>
2024-12-18 10:59:03 +0530

    task completed

--- pyls/__main__.py
+++ pyls/__main__.py
@@ -2,6 +2,7 @@ import json
 import argparse
 
 from pyls.ls import ls
+from pyls.help import help_statement
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Python ls implementation", add_help=False)
@@ -11,19 +12,23 @@ if __name__ == "__main__":
     parser.add_argument("-r", action="store_true")
     parser.add_argument("-t", action="store_true")
     parser.add_argument("-h", action="store_true")
+    parser.add_argument("--help", action="store_true")
     parser.add_argument("--filter", choices=["file", "dir"])
     args = parser.parse_args()
-    with open("structure.json") as structure_file:
-        structure_data = json.load(structure_file)
-        print(
-            ls(
-                structure_data,
-                a=args.A,
-                l=args.l,
-                r=args.r,
-                t=args.t,
-                h=args.h,
-                filter=args.filter,
-                path=args.path
+    if args.help:
+        print(help_statement)
+    else:
+        with open("structure.json") as structure_file:
+            structure_data = json.load(structure_file)
+            print(
+                ls(
+                    structure_data,
+                    a=args.A,
+                    l=args.l,
+                    r=args.r,
+                    t=args.t,
+                    h=args.h,
+                    filter=args.filter,
+                    path=args.path
+                )
             )
-        )
